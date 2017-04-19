3dc57e9c41bac4757023ba9f0bf83f6b9bc38b70
Nianhao <986000602@qq.com>
2017-04-19 14:38:07 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/db/City.java
@@ -0,0 +1,50 @@
+package db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by nianhao on 2017/4/19.
+ */
+
+public class City extends DataSupport {
+
+    private int id;
+
+    private String cityName;
+
+    private String cityCode;
+
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
